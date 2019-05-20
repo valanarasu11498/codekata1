7935bb02efc860e7bd703484e06ed45b989d8379
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-20 12:36:41 +0530

    Create voworcons.c

---
+++ voworcons.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(isalpha(c))
+    {
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+        {
+            printf("Vowel");
+        }
+        else
+        {
+            printf("Consonant");
+        }
+    }
+    else
+    {
+        printf("invalid");
+    }
+    return 0;
+}
