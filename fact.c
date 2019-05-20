83eee61641c449689862b2a03c8d62064edb69fa
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-20 18:10:09 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n,fact=1;
+    scanf("%d",&n);
+    if(n==1)
+    {
+        printf("1");
+    }
+    else
+    {
+        while(n!=0)
+        {
+            fact=fact*n;
+            n=n-1;
+        }
+        printf("%d",fact);
+    }
+    return 0;
+}
