cbe09b1543c9699340aa0e688e5ab1e383745936
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-20 14:50:53 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n,i,flag=1;
+    scanf("%d",&n);
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            flag=0;
+        }
+        else
+        {
+            flag=flag+1;
+        }
+    }
+    if(flag==0)
+    {
+        printf("no");
+    }
+    else
+    {
+        printf("yes");
+    }
+    return 0;
+}
