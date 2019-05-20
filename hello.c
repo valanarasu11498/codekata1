69e5a50df46946624ee28fed03f6e50adeb6e915
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-20 13:22:05 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int y,i;
+    scanf("%d",&y);
+    if(y>0)
+    {
+        for(i=0;i<y;i++)
+        {
+            printf("Hello");
+            printf("\n");
+        }
+    }
+    else
+    {
+        return 0;
+    }
+    return 0;
+}
