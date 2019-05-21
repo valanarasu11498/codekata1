23d9618022b5bdc2f626771152966ca153508aa1
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-21 12:22:25 +0530

    Create strchecknum.c

---
+++ #include <stdio.h> #include <string.h> #include<stdlib.h> int main() {     char a[20];     int l,i,num=0;     gets(a);/strchecknum.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+int main()
+{
+    char a[20];
+    int l,i,num=0;
+    gets(a);
+    //scanf("%d",&a);
+    l=strlen(a);
+    for(i=0;i<l;i++)
+    {
+        if(a[i]>=48&&a[i]<=57||a[i]==46)
+        {
+            num=num+1;
+        }
+        else
+        {
+            num=0;
+            printf("no");
+            return 0;
+        }
+    }
+    if(num>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
