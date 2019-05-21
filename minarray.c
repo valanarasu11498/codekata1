56a8002cf1a1eda22d66a8435413f26535c75838
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-21 11:16:45 +0530

    Update minarray.c

--- minarray.c
+++ minarray.c
@@ -5,9 +5,13 @@ int main()
 {
     int a[20],i,min=0,size;
     scanf("%d",&size);
-    for(i=0;i<size;i++)
+     for(i=0;i<size;i++)
     {   
         scanf("%d",&a[i]);
+    }
+    min=a[0];
+    for(i=0;i<size;i++)
+    {   
         if(a[i]<min)
         {
             min=a[i];
