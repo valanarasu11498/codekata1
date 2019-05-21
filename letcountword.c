fceb69bfcc5005cfa121b20896bacfda3372b873
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-21 16:28:48 +0530

    Create letcountword.c

---
+++ letcountword.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100],i,c=0;
+    //scanf("%[^\n]s",s);
+    gets(s);
+    while(s[i]!='\0')
+    {
+        if(s[i]!=' ')
+        {
+            c=c+1;
+        }
+        i++;
+    }
+    printf("%d",c);
+    
+    return 0;
+}
