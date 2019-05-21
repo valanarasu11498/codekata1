ee1b8d6f2e1ea2788881c005b3fc75c93f59be5d
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-21 17:41:20 +0530

    Create strntimes.c

---
+++ strntimes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int i,n;
+    char c[10];
+    gets(c);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        puts(c);
+    }
+    return 0;
+}
