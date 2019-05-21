4a69b2fa8311596ef48a6e331f056a6e1e40d2c9
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-21 16:33:02 +0530

    Create wordcount.c

---
+++ wordcount.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100],i,c=0;
+    //scanf("%[^\n]s",s);
+    gets(s);
+    while(s[i]!='\0')
+    {
+        if(s[i]==' ')
+        {
+            c=c+1;
+        }
+        i++;
+    }
+    c=c+1;
+    printf("%d",c);
+    
+    return 0;
+}
