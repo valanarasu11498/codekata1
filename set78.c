05a53782e2a7a8851b1bc71c09443adbc3379cc9
valanarasu11498 <50818637+valanarasu11498@users.noreply.github.com>
2019-05-23 18:20:57 +0530

    Create set78.c

---
+++ set78.c
@@ -0,0 +1,19 @@
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+   
+    int n;
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
